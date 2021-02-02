00a7671e0dc706f51f767ceca4c9d65c350428d4
Josh Goldberg <josh@codecademy.com>
2021-02-02 18:16:21 -0500

    Initial commit 💎🙌🚀🌕

---
+++ index.tsx
@@ -0,0 +1,8 @@
+import loadable from "@loadable/component";
+import React from 'react';
+
+const MyAsyncLoadable = loadable(() => import("./async"));
+
+export const MyComponent = () => {
+  return <MyAsyncLoadable ref={() => {}} />;
+};
